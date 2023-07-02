2d234af4fe68c87f785f46ce291adf45c9971126
Riyahi <p.riyahi@azmagroup.ir>
2023-07-02 10:29:02 +0330

    Add ``send_sms`` with prsmsp

--- web/app/views.py
+++ web/app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
 from django.contrib import messages
+from prsmsp.panel import Panel
 
+from app.models import SmsPanel
 from app.forms import SmsPanelForm, SmsForm
 
 def index(request):
@@ -23,12 +26,33 @@ def add_panel(request):
     })
 
 def send_sms_with_panel(request):
+    resp = ""
 
     if request.method == "POST":
         form = SmsForm(request.POST)
 
         if form.is_valid():
             form.save()
+            data = form.cleaned_data
+            #return JsonResponse(data["panel"].panel, safe=False)
+            #panel_obj = SmsPanel.objects.get(data["panel"])
+            panel_obj = data["panel"]
+
+            if panel_obj.username and panel_obj.password:
+                auth = {
+                    'username': panel_obj.username,
+                    'password': panel_obj.password,
+                }
+            else:
+                auth = {
+                    'api_key': panel_obj.api_key
+                }
+
+            p = Panel.initiate(panel_obj.panel, **auth)
+            if data["originator"]:
+                resp = p.send_sms(data["receptor"], data["message"])
+            else:
+                resp = p.send_sms(data["receptor"], data["message"], data["originator"])
             messages.success(request, f"sms sent to {form.data.get('receptor','')} successfully")
         else:
             messages.error(request, "Invalid Form")
@@ -37,4 +61,5 @@ def send_sms_with_panel(request):
 
     return render(request, "send_sms.html", {
         "form": form,
+        "resp": resp or "",
     })
